63af16955317df6593b8d8cbd722163ea4980ea2
tulio34 <tulio34@live.com>
2017-10-26 13:36:12 -0200

    Update feed.ts
    Criação do objeto feed

--- src/pages/feed/feed.ts
+++ src/pages/feed/feed.ts
@@ -13,7 +13,15 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   selector: 'page-feed',
   templateUrl: 'feed.html',
 })
-export class FeedPage {
+export class FeedPage {.
+  public objeto_feed = {
+    titulo_feed:"Primeiro Usuário Cadastrado",
+    data_feed:"Novembro, 5, 1995",
+    descricao_feed:"Estou criando um app massa, essa é a minha descrição",
+    qtd_likes_feed:12,
+    qtd_comentario_feed: 4,
+    time_comment_feed:"11h atrás"
+  }
 
   public nome_usuario:string = "Usuário do Sistema";
   constructor(public navCtrl: NavController, public navParams: NavParams) {
